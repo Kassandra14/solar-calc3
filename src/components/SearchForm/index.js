29566d71256f363871bb102158bfa769de7a592b
@kcruit-salzberg <kassandra.cruit-salzberg@alsoenergy.com>
2021-06-07 20:13:10 -0600

    extra pages

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -21,7 +21,7 @@ function SearchForm(props) {
           value={props.search}
           onChange={props.handleInputChange}
           name="monthlyKwh"
-          type="number"
+          type="decimal"
           className="form-control"
           placeholder="Type in your monthly energy use in Kwh (you will need a utility bill"
           id="monthlyKwh"
